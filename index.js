dda3c5e3ab2921559364fa7fd40723068694d37c
vyetasolutions <vyeta.tech@gmail.com>
2026-06-21 13:59:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('The Digital Fortress Aviator Server is officially LIVE!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
